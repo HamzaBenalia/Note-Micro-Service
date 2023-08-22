dc4b5c25f86d3c8b258ec81211ca75702f6b2f28
HamzaBenalia <hamzabenalia93@gmail.com>
2023-08-22 15:51:35 +0200

    adding java doc to note micro service

--- src/main/java/com/mediscreen/notes/controllers/NoteController.java
+++ src/main/java/com/mediscreen/notes/controllers/NoteController.java
@@ -1,8 +1,8 @@
 package com.mediscreen.notes.controllers;
-
 import com.mediscreen.notes.exception.NoteNotFoundException;
 import com.mediscreen.notes.model.Note;
 import com.mediscreen.notes.services.NoteService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,41 +25,71 @@ public class NoteController {
     @Autowired
     private NoteService noteService;
 
+    @Operation(
+            description = "Create a note endPoint"
+    )
+
     @PostMapping
     public Note createNote(@Valid @RequestBody Note note) {
         log.info("note créer avec succée: {}", note);
         return noteService.createNote(note);
     }
 
+    @Operation(
+            description = "get the list of notes endPoint"
+    )
+
     @GetMapping
     public List<Note> getNotes() {
+        log.info("la list des notes trouvée");
         return noteService.getNotes();
     }
 
+    @Operation(
+            description = "get note by Id endPoint"
+    )
+
     @GetMapping("/{id}")
     public Note getNote(@PathVariable String id) throws NoteNotFoundException {
+        log.info("la note est trouvée via son :{}", id);
         return noteService.getById(id);
     }
 
-
+    @Operation(
+            description = "get the list of notes via a patientId endPoint"
+    )
     @GetMapping("/patient/{patientId}")
     public List<Note> getNotesByPatientId(@PathVariable String patientId) {
+        log.info("Chercher les notes via le : {}", patientId);
         return noteService.getNotesByPatientId(patientId);
     }
 
-
+    @Operation(
+            description = "update a note via the id endPoint"
+    )
     @PostMapping("/update/{id}")
     public Note updateNote(@Valid @PathVariable String id, @RequestBody Note note) throws NoteNotFoundException {
+        log.info("Mise à jour de la note via le : {}", id);
         return noteService.updateNote(id, note);
     }
 
+    @Operation(
+            description = "delete the note via it's id endPoint"
+    )
+
     @DeleteMapping("/{id}")
     public String deleteNote(@PathVariable String id) {
+        log.info("la note est supprimer via le :{}", id);
         return noteService.deleteNote(id);
     }
 
+    @Operation(
+            description = "delete the note via the patientId endPoint"
+    )
+
     @DeleteMapping("/patient/{patientId}")
     public void deleteNoteByPatientId(@PathVariable Integer patientId) {
+        log.info("la note est supprimer avec son patient via :{}", patientId);
          noteService.deleteNoteByPatientId(patientId);
     }
 }
--- src/main/java/com/mediscreen/notes/services/impl/NoteServiceImpl.java
+++ src/main/java/com/mediscreen/notes/services/impl/NoteServiceImpl.java
@@ -16,6 +16,12 @@ public class NoteServiceImpl implements NoteService {
     @Autowired
     private NoteRepository noteRepository;
 
+    /**
+     * this method is for creating a note on in a DB
+     * @param note
+     * @return : it returns a saved note
+     */
+
 
     public Note createNote(Note note) {
         note.setDate(LocalDate.now());
@@ -23,15 +29,36 @@ public class NoteServiceImpl implements NoteService {
         return noteRepository.save(note);
     }
 
+    /**
+     * This method is for listing all the notes created
+     * @return : it returns a list of notes
+     */
+
     public List<Note> getNotes() {
         return noteRepository.findAll();
     }
+
+
+    /**
+     * This method is to retrieve a note via a note id
+     * @param id
+     * @return it returns a note
+     * @throws NoteNotFoundException
+     */
 
     public Note getById(String id) throws NoteNotFoundException {
         return noteRepository.findById(id)
                 .orElseThrow(() -> new NoteNotFoundException("Note non trouvé avec l'ID : " + id));
 
     }
+
+    /**
+     * This methode is for updating a given note via the id
+     * @param id
+     * @param noteDetails
+     * @return
+     * @throws NoteNotFoundException
+     */
 
 
     public Note updateNote(String id, Note noteDetails) throws NoteNotFoundException {
@@ -45,16 +72,32 @@ public class NoteServiceImpl implements NoteService {
                 }).orElseThrow(() -> new NoteNotFoundException("Note non trouvé avec l'ID :  " + id));
     }
 
+    /**
+     * This method is for getting all the notes via a patient id
+     * @param patientId
+     * @return
+     */
 
     @Override
     public List<Note> getNotesByPatientId(String patientId) {
         return noteRepository.findByPatientId(patientId);
     }
 
+    /**
+     * This method is for deleting a note via an id
+     * @param id
+     * @return
+     */
+    @Override
     public String deleteNote(String id) {
         noteRepository.deleteById(id);
         return "Note supprimée";
     }
+
+    /**
+     * This method is for deleting a note that is attached to a patient via a patient id
+     * @param patientId
+     */
     @Override
     public void deleteNoteByPatientId(Integer patientId) {
         noteRepository.deleteByPatientId(String.valueOf(patientId));
